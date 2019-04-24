a77391d93261726088183c23f35c48c3183be07e
atiftariq786 <atiftariq786@hotmail.com>
2019-04-23 18:57:09 -0700

    Setup directory structure , code added in app.js and index.js

---
+++ memorygame/src/App.js
@@ -0,0 +1,18 @@
+import daenerys from "./images/daenerys.gif";
+import dragon from "./images/dragon.gif";
+import gameofthrone from "./images/gameofthrone.gif";
+import targaryen from "./images/targaryen.gif";
+import whitewalker from "./images/whitewalker.gif";
+
+
+const images =[
+
+daenerys,
+dragon,
+gameofthrone,
+targaryen,
+whitewalker
+
+]
+
+export default images;
