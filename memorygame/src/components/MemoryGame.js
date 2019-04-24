c81a30bfee3eff4dc181ae542aa4d1b3dcf06881
atiftariq786 <atiftariq786@hotmail.com>
2019-04-23 21:27:39 -0700

    footer code add in Footer.js inside components folder

--- memorygame/src/components/MemoryGame.js
+++ memorygame/src/components/MemoryGame.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Container from './Container';
-
+import Footer from './Footer';
+import Banner from './Banner';
 import images from '../images';
 
 class MemoryGame extends Component {
