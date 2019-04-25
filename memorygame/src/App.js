a104c53a5fb9e4cc6257d253dc92682dec36dff1
atiftariq786 <atiftariq786@hotmail.com>
2019-04-24 17:41:32 -0700

    Code added in app.js to import more images

--- memorygame/src/App.js
+++ memorygame/src/App.js
@@ -1,18 +1,32 @@
-import beth from "./images/beth.png";
 import daenerys from "./images/daenerys.gif";
 import dragon from "./images/dragon.gif";
 import gameofthrone from "./images/gameofthrone.gif";
 import targaryen from "./images/targaryen.gif";
 import whitewalker from "./images/whitewalker.gif";
+import got10 from "./images/got10.gif";
+import got11 from "./images/got11.gif";
+import got12 from "./images/got12.gif";
+import got13 from "./images/got13.gif";
+import got14 from "./images/got14.gif";
+import got15 from "./images/got15.gif";
+import got16 from "./images/got16.gif";
 
 
 const images =[
-beth,
+
 daenerys,
 dragon,
 gameofthrone,
 targaryen,
-whitewalker
+whitewalker,
+got10,
+got11,
+got12,
+got13,
+got14,
+got15,
+got16
+
 
 ]
 
